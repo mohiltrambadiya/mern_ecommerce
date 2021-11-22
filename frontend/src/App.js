546d5820472539b82f731cd825d6e617936bdcaf
mohiltrambadiya <mohiltrambadiya@gmail.com>
2021-11-22 11:13:18 +0530

    2021-11-22 front end initial set up and header, footer, home component

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Header from './components/layout/Header/Header.js';
+import {
+  BrowserRouter, Routes , Route 
+} from "react-router-dom";
+import Webfont from "webfontloader";
+import React from 'react';
+import Footer from './components/layout/Footer/Footer.js';
+import Home from './components/Home/Home.js';
+
+function App() {
+
+  React.useEffect(() => {
+    Webfont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka']
+      }
+    });
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
